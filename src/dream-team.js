b0a73410b410f89951ca5136af38c3d377d86afe
Anna <kowalewskajaanna@gmail.com>
2023-12-25 23:20:33 +0100

    fix: carbon, count cats, dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  const validNames = members.filter(name => typeof name === 'string');
+  const initials = validNames.map(name => name.trim()[0].toUpperCase());
+  return initials.sort().join('');
 }
 
 module.exports = {
