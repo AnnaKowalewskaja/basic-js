bbe27e09a74cd0c79840a4b8024a86555744925f
Anna <kowalewskajaanna@gmail.com>
2023-12-25 23:39:41 +0100

    fix:some tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    this.checkParameters(message, key);
+
+    message = message.toUpperCase();
+    key = this.generateKey(message, key);
+
+    let result = '';
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(message[i])) {
+        const messageIndex = this.alphabet.indexOf(message[i]);
+        const keyIndex = this.alphabet.indexOf(key[i]);
+        const encryptedCharIndex = (messageIndex + keyIndex) % 26;
+        result += this.alphabet[encryptedCharIndex];
+      } else {
+        result += message[i];
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    this.checkParameters(encryptedMessage, key);
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = this.generateKey(encryptedMessage, key);
+
+    let result = '';
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (this.alphabet.includes(encryptedMessage[i])) {
+        const encryptedMessageIndex = this.alphabet.indexOf(encryptedMessage[i]);
+        const keyIndex = this.alphabet.indexOf(key[i]);
+        const decryptedCharIndex = (encryptedMessageIndex - keyIndex + 26) % 26;
+        result += this.alphabet[decryptedCharIndex];
+      } else {
+        result += encryptedMessage[i];
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
+  }
+
+  checkParameters(message, key) {
+    if (typeof message !== 'string' || typeof key !== 'string') {
+      throw new Error('Incorrect arguments!');
+    }
+  }
+
+  generateKey(message, key) {
+    let generatedKey = '';
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (this.alphabet.includes(message[i])) {
+        generatedKey += key[j % key.length].toUpperCase();
+        j++;
+      } else {
+        generatedKey += message[i];
+      }
+    }
+    return generatedKey;
   }
 }
 
